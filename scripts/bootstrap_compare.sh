fe88992806055f683eecfc29db73bb3946d63ff2
J. Protopopovs <jprotopopov1122@gmail.com>
2022-07-17 23:04:57 +0300

    Adapt bootstrap comparison script for FreeBSD

--- scripts/bootstrap_compare.sh
+++ scripts/bootstrap_compare.sh
@@ -5,8 +5,12 @@ set -e
 STAGE1="$1"
 STAGE2="$2"
 
+if [[ "x$REALPATH" == "x" ]]; then
+    REALPATH="realpath"
+fi
+
 find "$STAGE1" -name "*.s" -print0 | while read -d $'\0' stage1file
 do
-    stage2file="$STAGE2/$(realpath --relative-to=$STAGE1 $stage1file)"
+    stage2file="$STAGE2/$($REALPATH --relative-to=$STAGE1 $stage1file)"
     diff -u "$stage1file" "$stage2file"
 done
