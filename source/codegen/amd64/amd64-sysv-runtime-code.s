8e5c0078e0b78e2b02e6c7b4b0fe4d8245f3ddeb
J. Protopopovs <jprotopopov1122@gmail.com>
2022-08-09 23:04:28 +0300

    Add missing gnu-stack section

--- source/codegen/amd64/amd64-sysv-runtime-code.s
+++ source/codegen/amd64/amd64-sysv-runtime-code.s
@@ -16,6 +16,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+.section .note.GNU-stack,"",%progbits
+
 .section .rodata
 
 .global KefirAmd64SysVRuntimeCode
