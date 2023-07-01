4b768898584cf521a810fbfe6d18243f13958a24
J. Protopopovs <jevgenij@protopopov.lv>
2023-07-01 10:49:35 +0200

    Fix stack disbalance in switch statement

--- source/ast-translator/nodes/switch_statement.c
+++ source/ast-translator/nodes/switch_statement.c
@@ -58,6 +58,7 @@ kefir_result_t kefir_ast_translate_switch_statement_node(struct kefir_mem *mem,
                                                                      KEFIR_IRBUILDER_BLOCK_CURRENT_INDEX(builder) - 1));
     }
 
+    REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IROPCODE_POP, 0));
     REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IROPCODE_JMP, 0));
     if (flow_control_stmt->value.switchStatement.defaultCase != NULL) {
         REQUIRE_OK(kefir_ast_translator_flow_control_point_reference(
