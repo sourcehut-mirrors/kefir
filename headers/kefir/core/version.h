57383efdd5d1465d17a349b7edece3f3d461ebd5
J. Protopopovs <jevgenij@protopopov.lv>
2023-08-05 12:48:52 +0200

    Bump kefir version

--- headers/kefir/core/version.h
+++ headers/kefir/core/version.h
@@ -1,10 +1,30 @@
+/*
+    SPDX-License-Identifier: GPL-3.0
+
+    Copyright (C) 2020-2023  Jevgenijs Protopopovs
+
+    This file is part of Kefir project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef KEFIR_CORE_VERSION_H_
 #define KEFIR_CORE_VERSION_H_
 
 #include "kefir/core/base.h"
 
 #define KEFIR_VERSION_MAJOR 0
-#define KEFIR_VERSION_MINOR 2
+#define KEFIR_VERSION_MINOR 3
 #define KEFIR_VERSION_PATCH 0
 
 #endif
