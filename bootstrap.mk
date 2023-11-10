a38fcc9bab1d0ab9a2bef51735d22f93601afecf
J. Protopopovs <jevgenij@protopopov.lv>
2023-11-10 10:08:18 +0200

    Enable bootstrap with the new code generator

--- bootstrap.mk
+++ bootstrap.mk
@@ -12,11 +12,11 @@ KEFIR_LD=ld
 
 BOOTSTRAP_CFLAGS=-I $(HEADERS)
 ifeq ($(PLATFORM),freebsd)
-BOOTSTRAP_CFLAGS += --target x86_64-freebsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1
+BOOTSTRAP_CFLAGS += --target new-x86_64-freebsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1
 else ifeq ($(PLATFORM),openbsd)
-BOOTSTRAP_CFLAGS += --target x86_64-openbsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1 -include $(HEADERS)/bootstrap_include/openbsd.h
+BOOTSTRAP_CFLAGS += --target new-x86_64-openbsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1 -include $(HEADERS)/bootstrap_include/openbsd.h
 else ifeq ($(PLATFORM),netbsd)
-BOOTSTRAP_CFLAGS += --target x86_64-netbsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1 -D__ELF__=1 -include $(HEADERS)/bootstrap_include/netbsd.h
+BOOTSTRAP_CFLAGS += --target new-x86_64-netbsd-system -D__GNUC__=4 -D__GNUC_MINOR__=20 -D__GNUC_STDC_INLINE__=1 -D__ELF__=1 -include $(HEADERS)/bootstrap_include/netbsd.h
 else
 BOOTSTRAP_CFLAGS += --target new-x86_64-linux-gnu
 endif
