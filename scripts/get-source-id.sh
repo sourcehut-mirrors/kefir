5ee97ab4eb70242229839b9e69119f76b95e3895
J. Protopopovs <jevgenij@protopopov.lv>
2024-01-16 18:23:11 +0200

    Update source ID generator script

--- scripts/get-source-id.sh
+++ scripts/get-source-id.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-git rev-parse HEAD 2>/dev/null || true
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    printf "r%s.%s" "$(git rev-list --count HEAD 2>/dev/null)" "$(git rev-parse HEAD  2>/dev/null)"
+fi
