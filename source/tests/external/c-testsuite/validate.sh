3a06b223b388eb5bc14d0199812569648865b21a
J. Protopopovs <jevgenij@protopopov.lv>
2024-04-13 11:30:43 +0200

    Fix c-testsuite validation on NetBSD

--- source/tests/external/c-testsuite/validate.sh
+++ source/tests/external/c-testsuite/validate.sh
@@ -6,6 +6,6 @@ C_TESTSUTE_DIR="$2"
 SUMMARY=`cat "$LOG_FILE" | "$C_TESTSUTE_DIR/scripts/tapsummary"`
 echo "$SUMMARY"
 
-if [[ "x$(echo "$SUMMARY" | sed -nr 's/fail\s*([0-9]+)/\1/p')" != "x0"  ]]; then
+if [[ "x$(echo "$SUMMARY" | sed -nr 's/fail[ ]*([0-9]+)/\1/p')" != "x0"  ]]; then
     exit 1
 fi
