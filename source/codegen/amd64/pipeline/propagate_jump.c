9f2459b4be28292b61bacedf7f726c1ff19023f9
J. Protopopovs <jevgenij@protopopov.lv>
2024-04-13 12:45:28 +0200

    Fix infinite loop in jump propagation pass

--- source/codegen/amd64/pipeline/propagate_jump.c
+++ source/codegen/amd64/pipeline/propagate_jump.c
@@ -23,13 +23,9 @@
 #include "kefir/codegen/amd64/asmcmp.h"
 #include "kefir/core/error.h"
 #include "kefir/core/util.h"
+#include <string.h>
 
-static kefir_result_t propagate_jump_apply(struct kefir_mem *mem, struct kefir_asmcmp_context *context,
-                                 const struct kefir_asmcmp_pipeline_pass *pass) {
-    UNUSED(mem);
-    UNUSED(context);
-    UNUSED(pass);
-
+static kefir_result_t propagate_jump_impl(struct kefir_mem *mem, struct kefir_asmcmp_context *context, struct kefir_hashtreeset *jump_chain) {
     for (kefir_asmcmp_instruction_index_t instr_index = kefir_asmcmp_context_instr_head(context), next_instr_index;
          instr_index != KEFIR_ASMCMP_INDEX_NONE; instr_index = next_instr_index) {
 
@@ -61,18 +57,40 @@ static kefir_result_t propagate_jump_apply(struct kefir_mem *mem, struct kefir_a
                     REQUIRE_OK(kefir_asmcmp_context_label_at(context, instr->args[0].internal_label, &jump_target_instr_idx));
                     REQUIRE_OK(kefir_asmcmp_context_instr_at(context, jump_target_instr_idx, &jump_target_instr));
 
-                    if (jump_target_instr->opcode == KEFIR_ASMCMP_AMD64_OPCODE(jmp)) {
+                    if (jump_target_instr->opcode == KEFIR_ASMCMP_AMD64_OPCODE(jmp) && !kefir_hashtreeset_has(jump_chain, jump_target_instr_idx)) {
                         instr->args[0] = jump_target_instr->args[0];
                         next_instr_index = instr_index;
+                        REQUIRE_OK(kefir_hashtreeset_add(mem, jump_chain, (kefir_hashtreeset_entry_t) jump_target_instr_idx));
+                    } else {
+                        REQUIRE_OK(kefir_hashtreeset_clean(mem, jump_chain));
                     }
+                } else {
+                    REQUIRE_OK(kefir_hashtreeset_clean(mem, jump_chain));
                 }
                 break;
 
             default:
-                // Intentionally left blank
+                REQUIRE_OK(kefir_hashtreeset_clean(mem, jump_chain));
                 break;
         }
     }
+    return KEFIR_OK;
+}
+
+static kefir_result_t propagate_jump_apply(struct kefir_mem *mem, struct kefir_asmcmp_context *context,
+                                 const struct kefir_asmcmp_pipeline_pass *pass) {
+    UNUSED(mem);
+    UNUSED(context);
+    UNUSED(pass);
+
+    struct kefir_hashtreeset jump_chain;
+    REQUIRE_OK(kefir_hashtreeset_init(&jump_chain, &kefir_hashtree_uint_ops));
+    kefir_result_t res = propagate_jump_impl(mem, context, &jump_chain);
+    REQUIRE_ELSE(res == KEFIR_OK, {
+        kefir_hashtreeset_free(mem, &jump_chain);
+        return res;
+    });
+    REQUIRE_OK(kefir_hashtreeset_free(mem, &jump_chain));
     return KEFIR_OK;
 }
 
