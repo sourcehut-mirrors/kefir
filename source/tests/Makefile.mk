df36550812c7e791302f120dccdc4fe436ad02f4
J. Protopopovs <jevgenij@protopopov.lv>
2024-09-10 12:49:07 +0300

    Disable atomic tests for Musl builds

--- source/tests/Makefile.mk
+++ source/tests/Makefile.mk
@@ -1,5 +1,6 @@
 VALGRIND_TEST_OPTIONS=-q --trace-children=no --track-origins=yes --leak-check=full --error-exitcode=127
 TEST_CFLAGS=-no-pie
+KEFIR_TEST_USE_MUSL=no
 
 ifeq ($(PLATFORM),freebsd)
 VALGRIND_TEST_OPTIONS+=--suppressions=$(ROOT)/suppressions/freebsd.valgrind
