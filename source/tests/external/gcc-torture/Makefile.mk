c057760cbc950972c83af5112b24ebab102d8a5e
J. Protopopovs <jevgenij@protopopov.lv>
2024-12-14 21:28:00 +0200

    Update Torture test script

--- source/tests/external/gcc-torture/Makefile.mk
+++ source/tests/external/gcc-torture/Makefile.mk
@@ -36,7 +36,7 @@ $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_DIR)/torture.log: $(KEFIR_EXTERNAL_TEST_GCC_DI
 		KEFIR_RTLIB="$(realpath $(LIBKEFIRRT_A))" \
 		KEFIR_RTINC="$(realpath $(HEADERS_DIR)/runtime)" \
 		KEFIR_EXTRAFLAGS="$(KEFIR_EXTERNAL_TEST_GCC_TORTURE_CFLAGS)" \
-		bash -c 'set -o pipefail; "$(SOURCE_DIR)/tests/external/gcc-torture/run_gcc_torture_suite.sh" 2>&1 | tee "$@.tmp"'
+		bash -c 'set -o pipefail; "$(SOURCE_DIR)/tests/external/gcc-torture/run_gcc_torture_suite.sh" 2>&1 | (tee "$@.tmp" || true)'
 	@mv "$@.tmp" "$@"
 
 $(KEFIR_EXTERNAL_TESTS_DIR)/gcc-torture.test.done: $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_DIR)/torture.log
