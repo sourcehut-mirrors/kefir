05dbd170b1001f90afbc93daced77aaaac82b1c2
J. Protopopovs <jevgenij@protopopov.lv>
2025-01-12 15:45:27 +0200

    Update php external test validator script

--- source/tests/external/php/validate.sh
+++ source/tests/external/php/validate.sh
@@ -6,6 +6,3 @@ if ! grep -E "^Tests failed\s*:\s*0\s*\(\s*0.0%\s*\)\s*\(\s*0.0%\s*\)$" "$LOG_FI
     exit 1
 fi
 
-if ! grep -E "^Tests passed\s*:\s*13517" "$LOG_FILE" >/dev/null; then
-    exit 1
-fi
