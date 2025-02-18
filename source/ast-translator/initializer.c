65a5d220ca952bcd68f8f6af670e3ebc975b5ad3
J. Protopopovs <jevgenij@protopopov.lv>
2025-02-18 08:35:36 +0200

    Update aggregate initialization to perform zeroing only once

--- source/ast-translator/initializer.c
+++ source/ast-translator/initializer.c
@@ -73,7 +73,7 @@ static kefir_result_t kefir_ast_translate_initializer_impl(struct kefir_mem *, s
                                                            struct kefir_irbuilder_block *,
                                                            const struct kefir_ast_type *,
                                                            const struct kefir_ast_initializer *,
-                                                           struct kefir_hashtreeset *);
+                                                           struct kefir_hashtreeset *, kefir_bool_t);
 
 static kefir_result_t initialize_aggregate_with_scalar(struct kefir_mem *mem,
                                                        struct kefir_ast_translator_context *context,
@@ -101,7 +101,7 @@ static kefir_result_t initialize_aggregate_with_scalar(struct kefir_mem *mem,
                                        &virtual_expr_initializer_entry));
 
     kefir_result_t res = kefir_ast_translate_initializer_impl(mem, context, builder, type_layout->type,
-                                                              &virtual_initializer, repeated_expressions);
+                                                              &virtual_initializer, repeated_expressions, true);
     REQUIRE_ELSE(res == KEFIR_OK, {
         kefir_list_free(mem, &virtual_initializer.list.initializers);
         return res;
@@ -185,12 +185,12 @@ static kefir_result_t traverse_string_literal(const struct kefir_ast_designator 
         type_layout = param->translator_type->object.layout;
     }
 
-    if (type_layout->type->array_type.boundary == KEFIR_AST_ARRAY_BOUNDED || type_layout->type->array_type.boundary == KEFIR_AST_ARRAY_BOUNDED_STATIC) {
+    if (type_layout->type->array_type.boundary == KEFIR_AST_ARRAY_BOUNDED ||
+        type_layout->type->array_type.boundary == KEFIR_AST_ARRAY_BOUNDED_STATIC) {
         length = MIN(length, kefir_ast_type_array_const_length(&type_layout->type->array_type));
     }
 
     REQUIRE_OK(translate_address(param->translator_type, designator, param->builder));
-    REQUIRE_OK(zero_type(param->builder, param->translator_type->object.ir_type_id, type_layout));
     const struct kefir_ast_type *array_type = kefir_ast_type_array(
         param->mem, param->context->ast_context->type_bundle, type_layout->type->array_type.element_type,
         kefir_ast_constant_expression_integer(param->mem, length), &type_layout->type->array_type.qualifications);
@@ -199,24 +199,6 @@ static kefir_result_t traverse_string_literal(const struct kefir_ast_designator 
     REQUIRE_OK(kefir_ast_translator_store_value(
         param->mem, KEFIR_AST_TYPE_SAME(type_layout->type, array_type) ? type_layout->qualified_type : array_type,
         param->context, param->builder, &expression->source_location));
-    return KEFIR_OK;
-}
-
-static kefir_result_t traverse_aggregate_union(const struct kefir_ast_designator *designator,
-                                               const struct kefir_ast_initializer *initializer, void *payload) {
-    UNUSED(initializer);
-    REQUIRE(payload != NULL, KEFIR_SET_ERROR(KEFIR_INTERNAL_ERROR, "Expected valid payload"));
-    ASSIGN_DECL_CAST(struct traversal_param *, param, payload);
-
-    struct kefir_ast_type_layout *type_layout = NULL;
-    if (designator != NULL) {
-        REQUIRE_OK(
-            kefir_ast_type_layout_resolve(param->translator_type->object.layout, designator, &type_layout, NULL, NULL));
-    } else {
-        type_layout = param->translator_type->object.layout;
-    }
-
-    REQUIRE_OK(zero_type(param->builder, param->translator_type->object.ir_type_id, type_layout));
     return KEFIR_OK;
 }
 
@@ -236,22 +218,23 @@ static kefir_result_t traverse_initializer_list(const struct kefir_ast_designato
 
     REQUIRE_OK(translate_address(param->translator_type, designator, param->builder));
     REQUIRE_OK(kefir_ast_translate_initializer_impl(param->mem, param->context, param->builder, type_layout->type,
-                                                    initializer, param->repeated_expressions));
+                                                    initializer, param->repeated_expressions, true));
     REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(param->builder, KEFIR_IROPCODE_POP, 0));
     return KEFIR_OK;
 }
 
-static kefir_result_t kefir_ast_translate_initializer_impl(struct kefir_mem *mem,
-                                                           struct kefir_ast_translator_context *context,
-                                                           struct kefir_irbuilder_block *builder,
-                                                           const struct kefir_ast_type *type,
-                                                           const struct kefir_ast_initializer *initializer,
-                                                           struct kefir_hashtreeset *repeated_expressions) {
+static kefir_result_t kefir_ast_translate_initializer_impl(
+    struct kefir_mem *mem, struct kefir_ast_translator_context *context, struct kefir_irbuilder_block *builder,
+    const struct kefir_ast_type *type, const struct kefir_ast_initializer *initializer,
+    struct kefir_hashtreeset *repeated_expressions, kefir_bool_t nested) {
     REQUIRE(mem != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid memory allocator"));
     REQUIRE(context != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid AST translator context"));
     REQUIRE(builder != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid IR block builder"));
     REQUIRE(type != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid AST type"));
     REQUIRE(initializer != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid AST initializer"));
+
+    const struct kefir_ast_type *unqualified_type = kefir_ast_unqualified_type(type);
+    REQUIRE(unqualified_type != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_STATE, "Unable to obtain unqualified AST type"));
 
     struct traversal_param param = {.mem = mem,
                                     .context = context,
@@ -261,17 +244,20 @@ static kefir_result_t kefir_ast_translate_initializer_impl(struct kefir_mem *mem
     REQUIRE_OK(kefir_ast_translator_type_new(mem, context->ast_context, context->environment, context->module, type, 0,
                                              &param.translator_type, &initializer->source_location));
 
+    kefir_result_t res = KEFIR_OK;
+    if (!nested && KEFIR_AST_TYPE_IS_AGGREGATE_TYPE(unqualified_type)) {
+        res = zero_type(builder, param.translator_type->object.ir_type_id, param.translator_type->object.layout);
+    }
+
     struct kefir_ast_initializer_traversal initializer_traversal;
     KEFIR_AST_INITIALIZER_TRAVERSAL_INIT(&initializer_traversal);
     initializer_traversal.visit_value = traverse_scalar;
     initializer_traversal.visit_string_literal = traverse_string_literal;
-    initializer_traversal.begin_struct_union = traverse_aggregate_union;
-    initializer_traversal.begin_array = traverse_aggregate_union;
     initializer_traversal.visit_initializer_list = traverse_initializer_list;
     initializer_traversal.payload = &param;
 
-    kefir_result_t res =
-        kefi_ast_traverse_initializer(mem, context->ast_context, initializer, type, &initializer_traversal);
+    REQUIRE_CHAIN(&res,
+                  kefi_ast_traverse_initializer(mem, context->ast_context, initializer, type, &initializer_traversal));
     REQUIRE_ELSE(res == KEFIR_OK, {
         kefir_ast_translator_type_free(mem, param.translator_type);
         return res;
@@ -293,7 +279,7 @@ kefir_result_t kefir_ast_translate_initializer(struct kefir_mem *mem, struct kef
     REQUIRE_OK(kefir_hashtreeset_init(&repeated_expressions, &kefir_hashtree_uint_ops));
 
     kefir_result_t res =
-        kefir_ast_translate_initializer_impl(mem, context, builder, type, initializer, &repeated_expressions);
+        kefir_ast_translate_initializer_impl(mem, context, builder, type, initializer, &repeated_expressions, false);
     REQUIRE_ELSE(res == KEFIR_OK, {
         kefir_hashtreeset_free(mem, &repeated_expressions);
         return res;
