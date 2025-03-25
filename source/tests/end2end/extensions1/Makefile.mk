222083efe2de462505bffee9f6b28c09b73b784e
J. Protopopovs <jevgenij@protopopov.lv>
2025-03-25 20:26:59 +0200

    Update extension library end2end test

--- source/tests/end2end/extensions1/Makefile.mk
+++ source/tests/end2end/extensions1/Makefile.mk
@@ -1,9 +1,15 @@
 ifeq ($(USE_EXTENSION_SUPPORT),yes)
 $(KEFIR_END2END_BIN_PATH)/extensions1/lib.kefir.o: $(KEFIR_END2END_BIN_PATH)/extensions1_lib.so
 
+ifeq ($(shell echo "__KEFIRCC__" | $(CC) -E -o- - | tr -d '\n'),1)
+KEFIR_END2END_EXTENSIONS1_LIB_EXTRA_CFLAGS := --disable-atomics
+else
+KEFIR_END2END_EXTENSIONS1_LIB_EXTRA_CFLAGS :=
+endif
+
 $(KEFIR_END2END_BIN_PATH)/extensions1_lib.so: $(SOURCE_DIR)/tests/end2end/extensions1/extension.c
 	@echo "Building $@"
-	@$(CC) $(CFLAGS) -I$(HEADERS_DIR) -include "$(KEFIR_HOST_ENV_CONFIG_HEADER)" -fPIC -shared $< -o $@
+	@$(CC) $(CFLAGS) -I$(HEADERS_DIR) -include "$(KEFIR_HOST_ENV_CONFIG_HEADER)" $(KEFIR_END2END_EXTENSIONS1_LIB_EXTRA_CFLAGS) -fPIC -shared $< -o $@ $(LDFLAGS)
 else
 KEFIR_END2END_EXCLUDE_TESTS+=$(SOURCE_DIR)/tests/end2end/extensions1/%
 endif
