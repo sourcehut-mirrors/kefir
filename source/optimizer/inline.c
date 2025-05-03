f9445fdf594b58cb977b530b9f0cee54dc0585b8
J. Protopopovs <jevgenij@protopopov.lv>
2025-05-03 22:04:10 +0300

    Do not omit local lifetime markings in inlined functions

--- source/optimizer/inline.c
+++ source/optimizer/inline.c
@@ -36,7 +36,6 @@ struct do_inline_param {
     kefir_opt_instruction_ref_t original_call_instr_ref;
     kefir_opt_phi_id_t result_phi_ref;
     kefir_opt_instruction_ref_t result_phi_instr;
-    kefir_bool_t skip_local_allocation_marks;
 
     struct kefir_hashtree block_mapping;
     struct kefir_hashtree instr_mapping;
@@ -696,22 +695,6 @@ static kefir_result_t do_inline_instr(kefir_opt_instruction_ref_t instr_ref, voi
                 KEFIR_SET_ERROR(KEFIR_INVALID_STATE,
                                 "Mismatch between inlined function argument count and call site arguments"));
         mapped_instr_ref = call_node->arguments[instr->operation.parameters.index];
-    } else if (instr->operation.opcode == KEFIR_OPT_OPCODE_LOCAL_LIFETIME_MARK && param->skip_local_allocation_marks) {
-        // Ignore local lifetime marks in inlined function, thus making respective variables alive for the complete
-        // duration of enclosing function
-        REQUIRE_OK(
-            kefir_opt_code_builder_int_placeholder(param->mem, param->dst_code, mapped_block_id, &mapped_instr_ref));
-
-        kefir_bool_t is_control_flow;
-        REQUIRE_OK(kefir_opt_code_instruction_is_control_flow(&param->src_function->code, instr_ref, &is_control_flow));
-        if (is_control_flow) {
-            kefir_opt_instruction_ref_t control_prev;
-            REQUIRE_OK(kefir_opt_instruction_prev_control(&param->src_function->code, instr_ref, &control_prev));
-            kefir_opt_instruction_ref_t mapped_control_prev;
-            REQUIRE_OK(get_instr_ref_mapping(param, control_prev, &mapped_control_prev));
-            REQUIRE_OK(kefir_opt_code_container_insert_control(param->dst_code, mapped_block_id, mapped_control_prev,
-                                                               mapped_instr_ref));
-        }
     } else {
         switch (instr->operation.opcode) {
 #define OPCODE_DEF(_id, _symbolic, _class)                                      \
@@ -992,10 +975,6 @@ static kefir_result_t inline_debug_allocation_info(struct do_inline_param *param
 }
 
 static kefir_result_t do_inline_impl(struct do_inline_param *param) {
-    const struct kefir_opt_call_node *call_node;
-    REQUIRE_OK(kefir_opt_code_container_call(param->dst_code, param->original_call_ref, &call_node));
-    param->skip_local_allocation_marks = true;
-
     REQUIRE_OK(inline_blocks(param));
     REQUIRE_OK(map_inlined_phis(param));
     REQUIRE_OK(link_inlined_entry_block(param));
@@ -1023,8 +1002,7 @@ static kefir_result_t do_inline(struct kefir_mem *mem, const struct kefir_opt_mo
                                     .original_call_ref = original_call_ref,
                                     .original_call_instr_ref = original_call_instr_ref,
                                     .result_phi_ref = KEFIR_ID_NONE,
-                                    .result_phi_instr = KEFIR_ID_NONE,
-                                    .skip_local_allocation_marks = false};
+                                    .result_phi_instr = KEFIR_ID_NONE};
     REQUIRE_OK(kefir_hashtree_init(&param.block_mapping, &kefir_hashtree_uint_ops));
     REQUIRE_OK(kefir_hashtree_init(&param.instr_mapping, &kefir_hashtree_uint_ops));
 
