02b8a64dcb32b731cb7a2c1ce12b607a52b7d7d5
J. Protopopovs <jevgenij@protopopov.lv>
2025-05-13 19:12:40 +0300

    Update GCC torture test runner

--- source/tests/external/gcc-torture/Makefile.mk
+++ source/tests/external/gcc-torture/Makefile.mk
@@ -34,12 +34,10 @@ $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_DIR)/torture.log: $(KEFIR_EXTERNAL_TEST_GCC_DI
 		LD_LIBRARY_PATH="$(realpath $(LIB_DIR)):$$LD_LIBRARY_PATH" \
 		KEFIR_RTINC="$(realpath $(HEADERS_DIR)/kefir/runtime)" \
 		KEFIR_EXTRAFLAGS="$(KEFIR_EXTERNAL_TEST_GCC_TORTURE_CFLAGS)" \
-		bash -c 'set -o pipefail; "$(SOURCE_DIR)/tests/external/gcc-torture/run_gcc_torture_suite.sh" 2>&1 | tee "$@.tmp"'
-	@mv "$@.tmp" "$@"
+		"$(SOURCE_DIR)/tests/external/gcc-torture/runner.sh" "$@"
 
 $(KEFIR_EXTERNAL_TESTS_DIR)/gcc-torture.test.done: $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_DIR)/torture.log
-	@echo "Checking GCC $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_VERSION) torture test suite results..."
-	@"$(SOURCE_DIR)/tests/external/gcc-torture/validate.sh" "$(KEFIR_EXTERNAL_TEST_GCC_TORTURE_DIR)/torture.log"
+	@echo "Succesfully validated GCC $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_VERSION) torture test suite results..."
 	@touch "$@"
 	@echo "GCC $(KEFIR_EXTERNAL_TEST_GCC_TORTURE_VERSION) torture test suite successfully finished"
 
---
+++ source/tests/external/gcc-torture/runner.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0
+# 
+# Copyright (C) 2020-2025  Jevgenijs Protopopovs
+# 
+# This file is part of Kefir project.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+# # 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -eo pipefail
+
+SCRIPT_DIR="$(dirname `realpath $0`)"
+OUT_FILE="$1"
+
+TMP_FILE="$(mktemp)"
+
+cleanup () {
+    rm -f "$TMP_FILE"
+}
+
+trap cleanup EXIT HUP INT QUIT PIPE TERM
+
+"$SCRIPT_DIR/run_gcc_torture_suite.sh" 2>&1 | tee "$TMP_FILE"
+"$SCRIPT_DIR/validate.sh" "$TMP_FILE"
+
+mv "$TMP_FILE" "$OUT_FILE"
