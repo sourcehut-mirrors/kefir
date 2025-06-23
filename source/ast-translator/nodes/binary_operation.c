b02dac8d298d4e13a0a229644c7eb07b098b98bc
J. Protopopovs <jevgenij@protopopov.lv>
2025-06-23 09:11:21 +0300

    Implement support for bit-precise integer addition

--- source/ast-translator/nodes/binary_operation.c
+++ source/ast-translator/nodes/binary_operation.c
@@ -118,8 +118,9 @@ static kefir_result_t translate_addition(struct kefir_mem *mem, struct kefir_ast
                 break;
 
             case KEFIR_AST_TYPE_DATA_MODEL_BITINT:
-                return KEFIR_SET_ERROR(KEFIR_NOT_IMPLEMENTED,
-                                       "Full bit-precise integer support is not implemented yet");
+                REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IR_OPCODE_BITINT_ADD,
+                                                           result_normalized_type->bitprecise.width));
+                break;
 
             default:
                 return KEFIR_SET_ERROR(KEFIR_INVALID_STATE, "Expected value of an integral type");
