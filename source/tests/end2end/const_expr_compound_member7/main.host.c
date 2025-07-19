7d1da4bdfa60045f30e0933a61ebb312b57394fc
J. Protopopovs <jevgenij@protopopov.lv>
2025-07-19 22:46:29 +0300

    Add a test for non-scalar union member access in constant expressions

---
+++ source/tests/end2end/const_expr_compound_member7/main.host.c
@@ -0,0 +1,46 @@
+/*
+    SPDX-License-Identifier: GPL-3.0
+
+    Copyright (C) 2020-2025  Jevgenijs Protopopovs
+
+    This file is part of Kefir project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include "./definitions.h"
+
+int main(void) {
+    assert(a.a == 100);
+    assert(fabs(a.b - 3.14159f) < 1e-5);
+    assert(fabsl(creall(a.c) - 384.0L) < 1e-7);
+    assert(fabsl(cimagl(a.c) + 4.564L) < 1e-7);
+    assert(a.d[0] == 0xcafe);
+    assert(a.d[1] == 0xbabe);
+    assert(strcmp(a.x, "Hello") == 0);
+
+    assert(b.b.a == ~100);
+    assert(fabs(b.b.b + 3.14159f) < 1e-5);
+    assert(fabsl(creall(b.b.c) + 384.0L) < 1e-7);
+    assert(fabsl(cimagl(b.b.c) - 4.564L) < 1e-7);
+    assert(b.b.d[0] == ~0xcafe);
+    assert(b.b.d[1] == ~0xbabe);
+    assert(strcmp(b.b.x, "Goodbye") == 0);
+    return EXIT_SUCCESS;
+}
