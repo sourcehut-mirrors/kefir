7e28cbf345ae341a433478d9477ba93bc055eab8
J. Protopopovs <jevgenij@protopopov.lv>
2025-08-27 20:49:13 +0300

    Implement bit-precise bootstrap on build artifact level

--- scripts/bootstrap_compare.sh
+++ scripts/bootstrap_compare.sh
@@ -14,3 +14,19 @@ do
     stage2file="$STAGE2/$($REALPATH --relative-to=$STAGE1 $stage1file)"
     diff -u "$stage1file" "$stage2file"
 done
+
+if [[ -f "$STAGE1/libkefir.so" ]]; then
+    stage1_sha256=`sha256sum "$STAGE1/libkefir.so" | cut -f1 -d' '`
+    stage2_sha256=`sha256sum "$STAGE2/libkefir.so" | cut -f1 -d' '`
+    if [[ "$stage1_sha256" != "$stage2_sha256" ]]; then
+        echo "libkefir.so sha256 mismatch"
+        exit 1
+    fi
+fi
+
+stage1_sha256=`sha256sum "$STAGE1/kefir" | cut -f1 -d' '`
+stage2_sha256=`sha256sum "$STAGE2/kefir" | cut -f1 -d' '`
+if [[ "$stage1_sha256" != "$stage2_sha256" ]]; then
+    echo "kefir sha256 mismatch"
+    exit 1
+fi
--- source/tests/bootstrap/bootstrap.mk
+++ source/tests/bootstrap/bootstrap.mk
@@ -117,16 +117,16 @@ include source/binary_headers.mk
 $(BOOTSTRAP)/libkefir.so: $(KEFIR_LIB_ASM_FILES)
 	@mkdir -p $(shell dirname "$@")
 	@echo "Linking $@"
-	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) $(BOOTSTRAP_CFLAGS) -shared $^ -o $@
+	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) $(BOOTSTRAP_CFLAGS) -shared -Wl,--build-id=none -Wl,--discard-all $^ -o $@
 
 ifeq ($(USE_SHARED),yes)
 $(BOOTSTRAP)/kefir: $(KEFIR_DRIVER_ASM_FILES) $(BOOTSTRAP)/libkefir.so
 	@echo "Linking $@"
-	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) -pie $(BOOTSTRAP_CFLAGS) $(KEFIR_DRIVER_ASM_FILES) -o $@ -L$(BOOTSTRAP) -lkefir
+	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) -pie $(BOOTSTRAP_CFLAGS) $(KEFIR_DRIVER_ASM_FILES) -Wl,--build-id=none -Wl,--discard-all -o $@ -L$(BOOTSTRAP) -lkefir
 else
 $(BOOTSTRAP)/kefir: $(KEFIR_ASM_FILES)
 	@echo "Linking $@"
-	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) $(BOOTSTRAP_CFLAGS) $(KEFIR_ASM_FILES) -o $@
+	@KEFIR_AS=$(KEFIR_AS) KEFIR_LD=$(KEFIR_LD) $(BOOTSTRAP_CC) $(BOOTSTRAP_CFLAGS) $(KEFIR_ASM_FILES) -Wl,--build-id=none -Wl,--discard-all -o $@
 endif
 
 bootstrap: $(BOOTSTRAP)/kefir
