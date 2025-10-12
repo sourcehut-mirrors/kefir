a5328b78c9a9e7ef6fad7953150c9de75ce52438
J. Protopopovs <jevgenij@protopopov.lv>
2025-10-12 17:32:41 +0300

    Disable Vaglrind for error recovery tests

--- source/tests/end2end/analyzer_error_recovery1/Makefile.mk
+++ source/tests/end2end/analyzer_error_recovery1/Makefile.mk
@@ -1,2 +1,4 @@
 KEFIR_END2END_TEST_NAME := $(patsubst source/tests/end2end/%/Makefile.mk,%,$(lastword $(MAKEFILE_LIST)))
 $(KEFIR_END2END_BIN_PATH)/$(KEFIR_END2END_TEST_NAME)/lib.test.asmgen.done: override USE_VALGRIND=no
+$(KEFIR_END2END_BIN_PATH)/$(KEFIR_END2END_TEST_NAME)/lib2.test.asmgen.done: override USE_VALGRIND=no
+$(KEFIR_END2END_BIN_PATH)/$(KEFIR_END2END_TEST_NAME)/lib3.test.asmgen.done: override USE_VALGRIND=no
