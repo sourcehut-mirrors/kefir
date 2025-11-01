f849ac4042f00cb8898ec93f4381f21a5e75d0fd
J. Protopopovs <jevgenij@protopopov.lv>
2025-11-01 11:23:43 +0200

    Implement int128 in-memory function argument support

---
+++ source/tests/end2end/int128_function_call2/main.host.c
@@ -0,0 +1,35 @@
+/*
+    SPDX-License-Identifier: GPL-3.0
+
+    Copyright (C) 2020-2025  Jevgenijs Protopopovs
+
+    This file is part of Kefir project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "./definitions.h"
+
+int main(void) {
+    for (unsigned long x = 0; x < 1000; x++) {
+        struct i128 res = test1((struct i128){{x, 0}});
+
+        assert(res.arr[0] == (x ^ (x + 1) ^ (x + 2) ^ (x + 3) ^ (x + 4) ^ (x + 5) ^ (x + 6) ^ (x + 7) ^ (x + 8) ^ (x + 9) ^ (x + 10) ^ (x + 11)));
+        assert(res.arr[1] == 0);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ source/tests/end2end/int128_function_call3/lib.kefir.c
@@ -0,0 +1,30 @@
+/*
+    SPDX-License-Identifier: GPL-3.0
+
+    Copyright (C) 2020-2025  Jevgenijs Protopopovs
+
+    This file is part of Kefir project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+static inline __int128 somefn(__int128 a, __int128 b, __int128 c, __int128 d,
+        __int128 e, __int128 f, __int128 g, __int128 h,
+        __int128 i, __int128 j, __int128 k, __int128 l) {
+    return a ^ b ^ c ^ d ^ e ^ f ^ g ^ h ^ i ^ j ^ k ^ l;
+}
+
+__int128 test1(__int128 x) {
+    return somefn(x, x + 1, x + 2, x + 3, x + 4, x + 5, x + 6, x + 7, x + 8, x + 9, x + 10, x + 11);
+}
+
