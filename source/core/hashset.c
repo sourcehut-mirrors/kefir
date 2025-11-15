c70fa1ac22fd8c14b0b95826ce7d3aa9fd4d40f8
J. Protopopovs <jevgenij@protopopov.lv>
2025-11-15 23:44:52 +0200

    Hash set clean up optimization

--- source/core/hashset.c
+++ source/core/hashset.c
@@ -103,9 +103,7 @@ kefir_result_t kefir_hashset_clear(struct kefir_mem *mem, struct kefir_hashset *
     REQUIRE(mem != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid memory allocator"));
     REQUIRE(hashset != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid hashset"));
 
-    for (kefir_size_t i = 0; i < hashset->capacity; i++) {
-        hashset->entries[i].state = KEFIR_HASHTABLE_ENTRY_EMPTY;
-    }
+    memset(hashset->entries, 0, sizeof(struct kefir_hashset_entry) * hashset->capacity);
     hashset->occupied = 0;
     return KEFIR_OK;
 }
