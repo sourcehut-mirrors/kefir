a9207fca3c935be451d55a2b44e230bfb3326000
J. Protopopovs <jevgenij@protopopov.lv>
2026-01-04 17:41:23 +0200

    Perform target IR DCE until fixpoint

--- source/codegen/target-ir/amd64/transform/dead_code.c
+++ source/codegen/target-ir/amd64/transform/dead_code.c
@@ -90,43 +90,49 @@ kefir_result_t kefir_codegen_target_ir_amd64_transform_dead_code_elimination(str
         [KEFIR_TARGET_IR_AMD64_OPCODE(dec)] = true
     };
 
-    for (kefir_size_t i = 0; i < kefir_codegen_target_ir_code_block_count(code); i++) {
-        kefir_codegen_target_ir_block_ref_t block_ref = kefir_codegen_target_ir_code_block_by_index(code, i);
+    kefir_bool_t reached_fixpoint = false;
 
-        for (kefir_codegen_target_ir_instruction_ref_t instr_ref = kefir_codegen_target_ir_code_block_control_head(code, block_ref);
-            instr_ref != KEFIR_ID_NONE;) {
-            const struct kefir_codegen_target_ir_instruction *instr;
-            REQUIRE_OK(kefir_codegen_target_ir_code_instruction(code, instr_ref, &instr));
-            if (!DEAD_CODE_CANDIDATE_OPCODES[instr->operation.opcode]) {
-                instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
-                continue;
-            }
+    for (; !reached_fixpoint;) {
+        reached_fixpoint = true;
+        for (kefir_size_t i = 0; i < kefir_codegen_target_ir_code_block_count(code); i++) {
+            kefir_codegen_target_ir_block_ref_t block_ref = kefir_codegen_target_ir_code_block_by_index(code, i);
 
-            struct kefir_codegen_target_ir_use_iterator use_iter;
-            kefir_bool_t has_uses = kefir_codegen_target_ir_code_use_iter(code, &use_iter, instr_ref, NULL, NULL) != KEFIR_ITERATOR_END;
-            
-            struct kefir_codegen_target_ir_value_iterator value_iter;
-            struct kefir_codegen_target_ir_value_ref value_ref;
-            const struct kefir_codegen_target_ir_value_type *value_type;
-            kefir_result_t res;
-            for (res = kefir_codegen_target_ir_code_value_iter(code, &value_iter, instr_ref, &value_ref, &value_type);
-                res == KEFIR_OK && !has_uses;
-                res = kefir_codegen_target_ir_code_value_next(&value_iter, &value_ref, &value_type)) {
-                if (value_type->kind == KEFIR_CODEGEN_TARGET_IR_VALUE_TYPE_INDIRECT ||
-                    value_type->constraint.type == KEFIR_CODEGEN_TARGET_IR_ALLOCATION_REQUIREMENT) {
-                    has_uses = true;
+            for (kefir_codegen_target_ir_instruction_ref_t instr_ref = kefir_codegen_target_ir_code_block_control_head(code, block_ref);
+                instr_ref != KEFIR_ID_NONE;) {
+                const struct kefir_codegen_target_ir_instruction *instr;
+                REQUIRE_OK(kefir_codegen_target_ir_code_instruction(code, instr_ref, &instr));
+                if (!DEAD_CODE_CANDIDATE_OPCODES[instr->operation.opcode]) {
+                    instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
+                    continue;
                 }
-            }
-            if (res != KEFIR_ITERATOR_END) {
-                REQUIRE_OK(res);
-            }
-            
-            if (!has_uses) {
-                kefir_codegen_target_ir_instruction_ref_t next_instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
-                REQUIRE_OK(kefir_codegen_target_ir_code_drop_instruction(mem, code, instr_ref));
-                instr_ref = next_instr_ref;
-            } else {
-                instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
+
+                struct kefir_codegen_target_ir_use_iterator use_iter;
+                kefir_bool_t has_uses = kefir_codegen_target_ir_code_use_iter(code, &use_iter, instr_ref, NULL, NULL) != KEFIR_ITERATOR_END;
+                
+                struct kefir_codegen_target_ir_value_iterator value_iter;
+                struct kefir_codegen_target_ir_value_ref value_ref;
+                const struct kefir_codegen_target_ir_value_type *value_type;
+                kefir_result_t res;
+                for (res = kefir_codegen_target_ir_code_value_iter(code, &value_iter, instr_ref, &value_ref, &value_type);
+                    res == KEFIR_OK && !has_uses;
+                    res = kefir_codegen_target_ir_code_value_next(&value_iter, &value_ref, &value_type)) {
+                    if (value_type->kind == KEFIR_CODEGEN_TARGET_IR_VALUE_TYPE_INDIRECT ||
+                        value_type->constraint.type == KEFIR_CODEGEN_TARGET_IR_ALLOCATION_REQUIREMENT) {
+                        has_uses = true;
+                    }
+                }
+                if (res != KEFIR_ITERATOR_END) {
+                    REQUIRE_OK(res);
+                }
+                
+                if (!has_uses) {
+                    kefir_codegen_target_ir_instruction_ref_t next_instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
+                    REQUIRE_OK(kefir_codegen_target_ir_code_drop_instruction(mem, code, instr_ref));
+                    instr_ref = next_instr_ref;
+                    reached_fixpoint = false;
+                } else {
+                    instr_ref = kefir_codegen_target_ir_code_control_next(code, instr_ref);
+                }
             }
         }
     }
