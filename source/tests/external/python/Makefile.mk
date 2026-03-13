ef178cd3972c0b6e6858c2f0e5b75bd44290aafc
J. Protopopovs <jevgenij@protopopov.lv>
2026-03-13 15:41:17 +0200

    Upgrade Python external test version

--- source/tests/external/python/Makefile.mk
+++ source/tests/external/python/Makefile.mk
@@ -1,13 +1,13 @@
 
 KEFIR_EXTERNAL_TEST_PYTHON_DIR := $(KEFIR_EXTERNAL_TESTS_DIR)/python
 
-KEFIR_EXTERNAL_TEST_PYTHON_VERSION := 3.13.7
+KEFIR_EXTERNAL_TEST_PYTHON_VERSION := 3.14.3
 KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME := python-$(KEFIR_EXTERNAL_TEST_PYTHON_VERSION).tar.xz
 KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE := $(KEFIR_EXTERNAL_TEST_PYTHON_DIR)/$(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME)
 KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR := $(KEFIR_EXTERNAL_TEST_PYTHON_DIR)/Python-$(KEFIR_EXTERNAL_TEST_PYTHON_VERSION)
 KEFIR_EXTERNAL_TEST_PYTHON_URL := https://www.python.org/ftp/python/$(KEFIR_EXTERNAL_TEST_PYTHON_VERSION)/Python-$(KEFIR_EXTERNAL_TEST_PYTHON_VERSION).tar.xz
 
-KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_SHA256 := 5462f9099dfd30e238def83c71d91897d8caa5ff6ebc7a50f14d4802cdaaa79a
+KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_SHA256 := a97d5549e9ad81fe17159ed02c68774ad5d266c72f8d9a0b5a9c371fe85d902b
 
 $(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE):
 	@mkdir -p $(dir $@)
@@ -20,6 +20,7 @@ $(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR)/.extracted: $(KEFIR_EXTERNAL_TEST_PYTHO
 	@echo "Extracting $(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME)..."
 	@cd "$(KEFIR_EXTERNAL_TEST_PYTHON_DIR)" && xz -d "$(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME)" --stdout > "$(basename $(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME))"
 	@cd "$(KEFIR_EXTERNAL_TEST_PYTHON_DIR)" && tar xvf "$(basename $(KEFIR_EXTERNAL_TEST_PYTHON_ARCHIVE_FILENAME))"
+	@cd "$(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR)" && patch -p0 < "$(realpath $(SOURCE_DIR))/tests/external/python/python.patch"
 	@touch "$@"
 
 $(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR)/Makefile: $(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR)/.extracted $(KEFIR_EXE)
@@ -28,6 +29,7 @@ $(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE_DIR)/Makefile: $(KEFIR_EXTERNAL_TEST_PYTHON_
 		LD_LIBRARY_PATH="$(realpath $(LIB_DIR))$(if $(LD_LIBRARY_PATH),:$(LD_LIBRARY_PATH))" \
 		KEFIR_RTINC="$(realpath $(HEADERS_DIR))/kefir/runtime" \
 		CC="$(realpath $(KEFIR_EXE))" \
+		CFLAGS="-isystem $(realpath $(SOURCE_DIR))/tests/external/python/include" \
 		LC_ALL=C.UTF-8 \
 		./configure
 
@@ -47,7 +49,7 @@ $(KEFIR_EXTERNAL_TEST_PYTHON_DIR)/tests.log: $(KEFIR_EXTERNAL_TEST_PYTHON_SOURCE
 		KEFIR_RTINC="$(realpath $(HEADERS_DIR))/kefir/runtime" \
 		CC="$(realpath $(KEFIR_EXE))" \
 		LC_ALL=C.UTF-8 \
-		bash -c "ulimit -s unlimited && set -o pipefail; ./python -m test -x test_gdb test_epoll test_external_inspection test_perf_profiler test_selectors test_ssl test_poplib test_socket test_socketserver | tee ../tests.log.tmp"
+		bash -c "set -o pipefail; ./python -m test -x test_gdb test_remote_pdb test_external_inspection  -i "test.test_sys.TestRemoteExec.*" -i test.test_audit.AuditTest.test_sys_remote_exec -i test.test_interpreters.test_queues.LowLevelTests.test_highlevel_reloaded -i test.test_interpreters.test_channels.LowLevelTests.test_highlevel_reloade | tee ../tests.log.tmp"
 	@mv "$@.tmp" "$@"
 
 $(KEFIR_EXTERNAL_TESTS_DIR)/python.test.done: $(KEFIR_EXTERNAL_TEST_PYTHON_DIR)/tests.log
