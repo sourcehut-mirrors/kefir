ebd37d1cff527eb68f1b8591311e11cbbe50aa40
J. Protopopovs <jevgenij@protopopov.lv>
2026-03-30 22:52:01 +0300

    Update dev env script

--- dist/kefir_dev_env.sh
+++ dist/kefir_dev_env.sh
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-source /opt/emsdk-3.1.74/emsdk_env.sh
-
 export PATH="/opt/make-4.4.1/bin:/opt/csmith/bin:$PATH"
 export CSMITH="/opt/csmith/bin/csmith"
 export LIBRARY_PATH="/usr/lib/x86_64-linux-gnu"
