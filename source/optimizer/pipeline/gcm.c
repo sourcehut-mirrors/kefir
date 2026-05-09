6c7bca3876d323509c686817503de151aa595898
J. Protopopovs <jevgenij@protopopov.lv>
2026-05-10 00:25:26 +0300

    Initial implementation of GCM

--- source/optimizer/pipeline/gcm.c
+++ source/optimizer/pipeline/gcm.c
@@ -28,12 +28,13 @@
 struct gcm_state {
     struct kefir_mem *mem;
     struct kefir_opt_code_container *code;
+    struct kefir_opt_code_debug_info *debug_info;
     struct kefir_opt_code_control_flow control_flow;
     struct kefir_opt_code_loop_collection loops;
 
     struct kefir_list queue;
-    struct kefir_hashtable early_schedule;
-    struct kefir_hashtable late_schedule;
+    struct kefir_hashset scheduled_early;
+    struct kefir_hashset scheduled_late;
 
     kefir_opt_block_id_t deepest_input_block_ref;
 };
@@ -42,7 +43,7 @@ static kefir_result_t gcm_schedule_early_inputs_phase1(kefir_opt_instruction_ref
     ASSIGN_DECL_CAST(struct gcm_state *, state, payload);
     REQUIRE(state != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid GCM state"));
 
-    if (!kefir_hashtable_has(&state->early_schedule, (kefir_hashtable_key_t) instr_ref)) {
+    if (!kefir_hashset_has(&state->scheduled_early, (kefir_hashset_key_t) instr_ref)) {
         kefir_uint64_t key = (kefir_uint32_t) instr_ref;
         REQUIRE_OK(kefir_list_insert_after(state->mem, &state->queue, NULL, (void *) (kefir_uptr_t) key));
     }
@@ -53,12 +54,14 @@ static kefir_result_t gcm_schedule_early_inputs_phase2(kefir_opt_instruction_ref
     ASSIGN_DECL_CAST(struct gcm_state *, state, payload);
     REQUIRE(state != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid GCM state"));
 
-    kefir_hashtable_value_t table_value;
-    REQUIRE_OK(kefir_hashtable_at(&state->early_schedule, (kefir_hashtable_key_t) instr_ref, &table_value));
+    const struct kefir_opt_instruction *instr;
+    REQUIRE_OK(kefir_opt_code_container_instr(state->code, instr_ref, &instr));
 
-    if (state->control_flow.blocks[state->deepest_input_block_ref].dominance_tree_level <
-        state->control_flow.blocks[table_value].dominance_tree_level) {
-        state->deepest_input_block_ref = table_value;
+    if (instr->block_id == state->code->gate_block) {
+        return KEFIR_YIELD;
+    } else if (state->control_flow.blocks[state->deepest_input_block_ref].dominance_tree_level <
+               state->control_flow.blocks[instr->block_id].dominance_tree_level) {
+        state->deepest_input_block_ref = instr->block_id;
     }
     return KEFIR_OK;
 }
@@ -87,12 +90,16 @@ static kefir_result_t gcm_schedule_early(struct gcm_state *state) {
         kefir_bool_t inputs_resolved = (key >> 32) != 0;
 
         REQUIRE_OK(kefir_list_pop(state->mem, &state->queue, iter));
-        if (kefir_hashtable_has(&state->early_schedule, (kefir_hashtable_key_t) instr_ref)) {
+        if (kefir_hashset_has(&state->scheduled_early, (kefir_hashset_key_t) instr_ref)) {
             continue;
         }
 
         const struct kefir_opt_instruction *instr;
-        REQUIRE_OK(kefir_opt_code_container_instr(state->code, instr_ref, &instr));
+        kefir_result_t res = kefir_opt_code_container_instr(state->code, instr_ref, &instr);
+        if (res == KEFIR_NOT_FOUND) {
+            continue;
+        }
+        REQUIRE_OK(res);
 
         if (!inputs_resolved) {
             kefir_uint64_t key = (1ull << 32) | (kefir_uint32_t) instr_ref;
@@ -103,19 +110,61 @@ static kefir_result_t gcm_schedule_early(struct gcm_state *state) {
             continue;
         }
 
-        kefir_bool_t is_control_flow;
-        REQUIRE_OK(kefir_opt_code_instruction_is_control_flow(state->code, instr_ref, &is_control_flow));
-        if (instr->operation.opcode == KEFIR_OPT_OPCODE_PHI || is_control_flow) {
-            REQUIRE_OK(kefir_hashtable_insert(state->mem, &state->early_schedule, (kefir_hashtable_key_t) instr_ref,
-                                              (kefir_hashtable_value_t) instr->block_id));
-        } else {
+        kefir_bool_t moveable;
+        REQUIRE_OK(kefir_opt_instruction_is_moveable(state->code, instr_ref, &moveable));
+        if (moveable) {
             state->deepest_input_block_ref = state->code->entry_point;
-            REQUIRE_OK(kefir_opt_instruction_extract_inputs(state->code, instr, true, gcm_schedule_early_inputs_phase2,
-                                                            state));
-            REQUIRE_OK(kefir_hashtable_insert(state->mem, &state->early_schedule, (kefir_hashtable_key_t) instr_ref,
-                                              (kefir_hashtable_value_t) state->deepest_input_block_ref));
+            kefir_result_t res =
+                kefir_opt_instruction_extract_inputs(state->code, instr, true, gcm_schedule_early_inputs_phase2, state);
+            if (res != KEFIR_YIELD) {
+                REQUIRE_OK(res);
+                REQUIRE_OK(kefir_opt_move_instruction(state->code, instr_ref, state->deepest_input_block_ref));
+            }
+        }
+        REQUIRE_OK(kefir_hashset_add(state->mem, &state->scheduled_early, (kefir_hashtable_key_t) instr_ref));
+    }
+    return KEFIR_OK;
+}
+
+static kefir_result_t do_schedule_instr(struct gcm_state *state, kefir_opt_instruction_ref_t instr_ref,
+                                        kefir_opt_block_id_t late_block_ref) {
+    REQUIRE(late_block_ref != KEFIR_ID_NONE, KEFIR_OK);
+
+    const struct kefir_opt_instruction *instr;
+    REQUIRE_OK(kefir_opt_code_container_instr(state->code, instr_ref, &instr));
+
+    kefir_opt_block_id_t best_block_ref = late_block_ref;
+    for (kefir_opt_block_id_t iter_ref = late_block_ref;;
+         iter_ref = state->control_flow.blocks[iter_ref].immediate_dominator) {
+        if (iter_ref == state->code->gate_block) {
+            return KEFIR_OK;
+        }
+        kefir_uint32_t iter_loop_level = 0, best_loop_level = 0;
+        struct kefir_opt_code_loop *loop;
+        kefir_result_t res = kefir_opt_code_loop_collection_find_loop(&state->loops, iter_ref, &loop);
+        if (res != KEFIR_NOT_FOUND) {
+            REQUIRE_OK(res);
+            iter_loop_level = loop->level;
+        }
+
+        res = kefir_opt_code_loop_collection_find_loop(&state->loops, best_block_ref, &loop);
+        if (res != KEFIR_NOT_FOUND) {
+            REQUIRE_OK(res);
+            best_loop_level = loop->level;
+        }
+
+        if (iter_loop_level < best_loop_level) {
+            best_block_ref = iter_ref;
+        }
+        if (iter_ref == instr->block_id) {
+            break;
         }
     }
+
+    if (best_block_ref != instr->block_id) {
+        REQUIRE_OK(kefir_opt_move_instruction(state->code, instr_ref, best_block_ref));
+    }
+
     return KEFIR_OK;
 }
 
@@ -123,10 +172,10 @@ static kefir_result_t gcm_schedule_late(struct gcm_state *state) {
     REQUIRE_OK(kefir_list_clear(state->mem, &state->queue));
 
     kefir_result_t res;
-    struct kefir_hashtable_iterator early_sched_iter;
-    kefir_hashtable_key_t iter_key;
-    for (res = kefir_hashtable_iter(&state->early_schedule, &early_sched_iter, &iter_key, NULL); res == KEFIR_OK;
-         res = kefir_hashtable_next(&early_sched_iter, &iter_key, NULL)) {
+    struct kefir_hashset_iterator early_sched_iter;
+    kefir_hashset_key_t iter_key;
+    for (res = kefir_hashset_iter(&state->scheduled_early, &early_sched_iter, &iter_key); res == KEFIR_OK;
+         res = kefir_hashset_next(&early_sched_iter, &iter_key)) {
         REQUIRE_OK(kefir_list_insert_after(state->mem, &state->queue, NULL,
                                            (void *) (kefir_uptr_t) (kefir_uint32_t) iter_key));
     }
@@ -141,12 +190,16 @@ static kefir_result_t gcm_schedule_late(struct gcm_state *state) {
         kefir_bool_t uses_resolved = (key >> 32) != 0;
 
         REQUIRE_OK(kefir_list_pop(state->mem, &state->queue, iter));
-        if (kefir_hashtable_has(&state->late_schedule, (kefir_hashtable_key_t) instr_ref)) {
+        if (kefir_hashset_has(&state->scheduled_late, (kefir_hashset_key_t) instr_ref)) {
             continue;
         }
 
         const struct kefir_opt_instruction *instr;
-        REQUIRE_OK(kefir_opt_code_container_instr(state->code, instr_ref, &instr));
+        kefir_result_t res = kefir_opt_code_container_instr(state->code, instr_ref, &instr);
+        if (res == KEFIR_NOT_FOUND) {
+            continue;
+        }
+        REQUIRE_OK(res);
 
         if (!uses_resolved && instr->operation.opcode != KEFIR_OPT_OPCODE_PHI) {
             kefir_uint64_t key = (1ull << 32) | (kefir_uint32_t) instr_ref;
@@ -156,7 +209,7 @@ static kefir_result_t gcm_schedule_late(struct gcm_state *state) {
             struct kefir_opt_instruction_use_iterator use_iter;
             for (res = kefir_opt_code_container_instruction_use_instr_iter(state->code, instr_ref, &use_iter);
                  res == KEFIR_OK; res = kefir_opt_code_container_instruction_use_next(&use_iter)) {
-                if (!kefir_hashtable_has(&state->late_schedule, (kefir_hashtable_key_t) use_iter.use_instr_ref)) {
+                if (!kefir_hashset_has(&state->scheduled_late, (kefir_hashset_key_t) use_iter.use_instr_ref)) {
                     REQUIRE_OK(
                         kefir_list_insert_after(state->mem, &state->queue, NULL,
                                                 (void *) (kefir_uptr_t) (kefir_uint32_t) use_iter.use_instr_ref));
@@ -168,17 +221,16 @@ static kefir_result_t gcm_schedule_late(struct gcm_state *state) {
             continue;
         }
 
-        kefir_bool_t is_control_flow;
-        REQUIRE_OK(kefir_opt_code_instruction_is_control_flow(state->code, instr_ref, &is_control_flow));
-        if (instr->operation.opcode == KEFIR_OPT_OPCODE_PHI || is_control_flow) {
-            REQUIRE_OK(kefir_hashtable_insert(state->mem, &state->late_schedule, (kefir_hashtable_key_t) instr_ref,
-                                              (kefir_hashtable_value_t) instr->block_id));
+        REQUIRE_OK(kefir_hashset_add(state->mem, &state->scheduled_late, (kefir_hashset_key_t) instr_ref));
+
+        kefir_bool_t moveable;
+        REQUIRE_OK(kefir_opt_instruction_is_moveable(state->code, instr_ref, &moveable));
+        if (!moveable) {
             continue;
         }
 
         kefir_opt_block_id_t late_schedule_block_ref = KEFIR_ID_NONE;
 
-        kefir_result_t res;
         struct kefir_opt_instruction_use_iterator use_iter;
         for (res = kefir_opt_code_container_instruction_use_instr_iter(state->code, instr_ref, &use_iter);
              res == KEFIR_OK; res = kefir_opt_code_container_instruction_use_next(&use_iter)) {
@@ -188,37 +240,36 @@ static kefir_result_t gcm_schedule_late(struct gcm_state *state) {
             if (use_instr->operation.opcode == KEFIR_OPT_OPCODE_PHI) {
                 struct kefir_opt_phi_node_link_iterator link_iter;
                 kefir_opt_block_id_t link_block_id;
-                for (res = kefir_opt_phi_node_link_iter(state->code, use_instr->id, &link_iter, &link_block_id, NULL);
-                     res == KEFIR_OK; res = kefir_opt_phi_node_link_next(&link_iter, &link_block_id, NULL)) {
-                    if (late_schedule_block_ref == KEFIR_ID_NONE) {
-                        late_schedule_block_ref = link_block_id;
-                    } else {
-                        REQUIRE_OK(kefir_opt_find_closest_common_dominator(
-                            &state->control_flow, late_schedule_block_ref, link_block_id, &late_schedule_block_ref));
+                kefir_opt_instruction_ref_t link_instr_ref;
+                for (res = kefir_opt_phi_node_link_iter(state->code, use_instr->id, &link_iter, &link_block_id,
+                                                        &link_instr_ref);
+                     res == KEFIR_OK; res = kefir_opt_phi_node_link_next(&link_iter, &link_block_id, &link_instr_ref)) {
+                    if (link_instr_ref == instr_ref) {
+                        if (late_schedule_block_ref == KEFIR_ID_NONE) {
+                            late_schedule_block_ref = link_block_id;
+                        } else {
+                            REQUIRE_OK(kefir_opt_find_closest_common_dominator(&state->control_flow,
+                                                                               late_schedule_block_ref, link_block_id,
+                                                                               &late_schedule_block_ref));
+                        }
                     }
                 }
                 if (res != KEFIR_ITERATOR_END) {
                     REQUIRE_OK(res);
                 }
             } else {
-                kefir_hashtable_value_t table_value;
-                REQUIRE_OK(kefir_hashtable_at(&state->late_schedule, (kefir_hashtable_key_t) use_iter.use_instr_ref,
-                                              &table_value));
-                ASSIGN_DECL_CAST(kefir_opt_block_id_t, block_id, table_value);
                 if (late_schedule_block_ref == KEFIR_ID_NONE) {
-                    late_schedule_block_ref = block_id;
+                    late_schedule_block_ref = use_instr->block_id;
                 } else {
                     REQUIRE_OK(kefir_opt_find_closest_common_dominator(&state->control_flow, late_schedule_block_ref,
-                                                                       block_id, &late_schedule_block_ref));
+                                                                       use_instr->block_id, &late_schedule_block_ref));
                 }
             }
         }
         if (res != KEFIR_ITERATOR_END) {
             REQUIRE_OK(res);
         }
-
-        REQUIRE_OK(kefir_hashtable_insert(state->mem, &state->late_schedule, (kefir_hashtable_key_t) instr_ref,
-                                          (kefir_hashtable_value_t) late_schedule_block_ref));
+        REQUIRE_OK(do_schedule_instr(state, instr_ref, late_schedule_block_ref));
     }
     return KEFIR_OK;
 }
@@ -240,31 +291,31 @@ static kefir_result_t global_code_motion_apply(struct kefir_mem *mem, struct kef
     REQUIRE(module != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid optimizer module"));
     REQUIRE(func != NULL, KEFIR_SET_ERROR(KEFIR_INVALID_PARAMETER, "Expected valid optimizer function"));
 
-    struct gcm_state state = {.mem = mem, .code = &func->code};
+    struct gcm_state state = {.mem = mem, .code = &func->code, .debug_info = &func->debug_info};
     REQUIRE_OK(kefir_opt_code_control_flow_init(&state.control_flow));
     REQUIRE_OK(kefir_opt_code_loop_collection_init(&state.loops));
     REQUIRE_OK(kefir_list_init(&state.queue));
-    REQUIRE_OK(kefir_hashtable_init(&state.early_schedule, &kefir_hashtable_uint_ops));
-    REQUIRE_OK(kefir_hashtable_init(&state.late_schedule, &kefir_hashtable_uint_ops));
+    REQUIRE_OK(kefir_hashset_init(&state.scheduled_early, &kefir_hashtable_uint_ops));
+    REQUIRE_OK(kefir_hashset_init(&state.scheduled_late, &kefir_hashtable_uint_ops));
 
     kefir_result_t res = gcm_impl(&state);
     REQUIRE_ELSE(res == KEFIR_OK, {
-        kefir_hashtable_free(mem, &state.late_schedule);
-        kefir_hashtable_free(mem, &state.early_schedule);
+        kefir_hashset_free(mem, &state.scheduled_late);
+        kefir_hashset_free(mem, &state.scheduled_early);
         kefir_list_free(mem, &state.queue);
         kefir_opt_code_loop_collection_free(mem, &state.loops);
         kefir_opt_code_control_flow_free(mem, &state.control_flow);
         return res;
     });
-    res = kefir_hashtable_free(mem, &state.late_schedule);
+    res = kefir_hashset_free(mem, &state.scheduled_late);
     REQUIRE_ELSE(res == KEFIR_OK, {
-        kefir_hashtable_free(mem, &state.early_schedule);
+        kefir_hashset_free(mem, &state.scheduled_early);
         kefir_list_free(mem, &state.queue);
         kefir_opt_code_loop_collection_free(mem, &state.loops);
         kefir_opt_code_control_flow_free(mem, &state.control_flow);
         return res;
     });
-    res = kefir_hashtable_free(mem, &state.early_schedule);
+    res = kefir_hashset_free(mem, &state.scheduled_early);
     REQUIRE_ELSE(res == KEFIR_OK, {
         kefir_list_free(mem, &state.queue);
         kefir_opt_code_loop_collection_free(mem, &state.loops);
