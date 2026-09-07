37eae30dfac5cd6e1f7b3f0fb1875618523b2d72
J. Protopopovs <jevgenij@protopopov.lv>
2026-09-07 21:53:43 +0300

    Update mbedtls external test validator

--- source/tests/external/mbedtls/validate.sh
+++ source/tests/external/mbedtls/validate.sh
@@ -2,6 +2,12 @@
 
 LOG_FILE="$1"
 
-if ! grep '0 tests failed out of 130' "$LOG_FILE" >/dev/null; then
-    exit 1
+if grep '0 tests failed out of 130' "$LOG_FILE" >/dev/null; then
+    exit 0
 fi
+
+if grep '100% tests passed out of 130' "$LOG_FILE" >/dev/null; then
+    exit 0
+fi
+
+exit 1
